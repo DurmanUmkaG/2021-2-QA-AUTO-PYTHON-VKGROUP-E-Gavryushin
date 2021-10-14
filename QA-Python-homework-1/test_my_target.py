fa47bea72324815ff6e291ff7cc0ad975ffbb0aa
Evgeniy Gavryushin <e.gavryushin1@gmail.com>
2021-10-14 20:21:47 +0500

    Добавил тест на логин
    Добавил фикстуру для логина

--- QA-Python-homework-1/test_my_target.py
+++ QA-Python-homework-1/test_my_target.py
@@ -1,1 +1,10 @@
+import pytest
+from base import BaseCase
 
+class Test(BaseCase):
+    @pytest.fixture(scope='function')
+    def login(self):
+        self.login_page.login()
+
+    def test_login(self, login):
+        assert self.driver.current_url == 'https://target.my.com/dashboard'
