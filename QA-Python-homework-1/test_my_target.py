d67494f810de8c64b3223239be493bdd526d31a5
Evgeniy Gavryushin <e.gavryushin1@gmail.com>
2021-10-14 21:19:04 +0500

    Добавил тест на logout

--- QA-Python-homework-1/test_my_target.py
+++ QA-Python-homework-1/test_my_target.py
@@ -1,10 +1,16 @@
 import pytest
 from base import BaseCase
 
+
 class Test(BaseCase):
     @pytest.fixture(scope='function')
     def login(self):
         self.login_page.login()
 
+    @pytest.mark.skip
     def test_login(self, login):
         assert self.driver.current_url == 'https://target.my.com/dashboard'
+
+    def test_logout(self, login):
+        self.main_page.logout()
+        assert self.driver.current_url == 'https://target.my.com/'
