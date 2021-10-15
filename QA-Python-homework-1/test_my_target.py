6e365e5a55465771577bc14a9f45c2e105352e7c
Evgeniy Gavryushin <e.gavryushin1@gmail.com>
2021-10-15 11:26:13 +0500

    Добавил pytest.mark.UI
    Добавил файл pytest.ini

--- QA-Python-homework-1/test_my_target.py
+++ QA-Python-homework-1/test_my_target.py
@@ -8,13 +8,16 @@ class Test(BaseCase):
     def login(self):
         self.login_page.login()
 
+    @pytest.mark.UI
     def test_login(self, login):
         assert self.driver.current_url == 'https://target.my.com/dashboard'
 
+    @pytest.mark.UI
     def test_logout(self, login):
         self.main_page.logout()
         assert self.driver.current_url == 'https://target.my.com/'
 
+    @pytest.mark.UI
     def test_change_contact_information(self, login):
         full_name, contact_phone_number = self.main_page.change_contact_information()
         changed_full_name = self.main_page.find(self.main_page.locators.FULLNAME_LOCATOR).get_attribute('value')
@@ -23,6 +26,7 @@ class Test(BaseCase):
         assert full_name == changed_full_name
         assert contact_phone_number == changed_contact_phone_number
 
+    @pytest.mark.UI
     @pytest.mark.parametrize(
         'page_locator, item_locator',
         [
