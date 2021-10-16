6ae5091b2c4e7b54d58533d9bce7a7cebc50b125
Evgeniy Gavryushin <e.gavryushin1@gmail.com>
2021-10-16 21:30:52 +0500

    Исправил локаторы на независящие от локализации

--- QA-Python-homework-1/ui/locators/login_locators.py
+++ QA-Python-homework-1/ui/locators/login_locators.py
@@ -2,6 +2,6 @@ from selenium.webdriver.common.by import By
 
 
 class LoginPageLocators:
-    LOG_IN_LOCATOR = (By.XPATH, '//div[contains(text(), "Log in")]')
+    LOG_IN_LOCATOR = (By.XPATH, '//div[contains(@class, "responseHead-module-button")]')
     USERNAME_LOCATOR = (By.NAME, 'email')
     PASSWORD_LOCATOR = (By.NAME, 'password')
--- QA-Python-homework-1/ui/locators/main_locators.py
+++ QA-Python-homework-1/ui/locators/main_locators.py
@@ -2,13 +2,13 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    BALANCE_LOCATOR = (By.XPATH, '//div[contains(text(), "Balance")]')
-    LOG_OF_LOCATOR = (By.XPATH, '//a[contains(text(), "Log off")]')
-    PROFILE_LOCATOR = (By.XPATH, '//a[contains(text(), "Profile")]')
+    BALANCE_LOCATOR = (By.XPATH, '//div[contains(@class, "right-module-infoWrap")]')
+    LOG_OF_LOCATOR = (By.XPATH, '//a[contains(@href, "logout")]')
+    PROFILE_LOCATOR = (By.XPATH, '//a[contains(@href, "profile")]')
     FULLNAME_LOCATOR = (By.XPATH, '//input[@class="input__inp js-form-element" and @maxlength="100"]')
     PHONE_NUMBER_LOCATOR = (By.XPATH, '//input[@class="input__inp js-form-element" and @maxlength="20"]')
-    SAVE_BUTTON_LOCATOR = (By.XPATH, '//div[contains(text(), "Save")]')
-    SAVE_NOTIFICATION_LOCATOR = (By.XPATH, '//div[contains(text(), "Information saved successfully")]')
-    AUDIENCES_LOCATOR = (By.XPATH, '//a[contains(text(), "Audiences")]')
-    AUDIENCE_SEGMENTS_LOCATOR = (By.XPATH, '//span[contains(text(), "Audience segments")]')
-    PROFILE_SETTINGS_LOCATOR = (By.XPATH, '//span[contains(text(), "Profile settings")]')
+    SAVE_BUTTON_LOCATOR = (By.XPATH, '//button[@class="button button_submit"]')
+    SAVE_NOTIFICATION_LOCATOR = (By.XPATH, '//div[@class="_notification__content js-notification-content"]')
+    AUDIENCES_LOCATOR = (By.XPATH, '//a[contains(@href, "segments")]')
+    AUDIENCE_SEGMENTS_LOCATOR = (By.XPATH, '//span[@class="left-nav__group__label"]')
+    PROFILE_SETTINGS_LOCATOR = (By.XPATH, '//span[@class="left-nav__group__label"]')
