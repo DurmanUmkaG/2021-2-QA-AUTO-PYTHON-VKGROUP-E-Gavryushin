0351633d745859392d67753bfb552ea6b186aae1
Evgeniy Gavryushin <e.gavryushin1@gmail.com>
2021-10-26 14:21:01 +0500

    Добавил метод клика на кнопку создания кампании
    Добавил локаторы для кнопки создания кампании

--- QA-Python-homework-2/ui/locators/main_locators.py
+++ QA-Python-homework-2/ui/locators/main_locators.py
@@ -2,4 +2,12 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators:
-    pass
+    CREATE_CAMPAIGN_BUTTON = (
+        By.XPATH,
+        '//div[contains(@class, "dashboard-module-createButtonWrap")]//div'
+    )
+
+    CREATE_CAMPAIGN_NEW_USER = (
+        By.XPATH,
+        '//a[@href="/campaign/new"]'
+    )
--- QA-Python-homework-2/ui/pages/main_page.py
+++ QA-Python-homework-2/ui/pages/main_page.py
@@ -1,6 +1,14 @@
+from selenium.common.exceptions import TimeoutException
+
 from ui.locators.main_locators import MainPageLocators
 from ui.pages.base_page import BasePage
 
 
 class MainPage(BasePage):
     locators = MainPageLocators
+
+    def click_create_campaign(self):
+        try:
+            self.click(self.locators.CREATE_CAMPAIGN_NEW_USER)
+        except TimeoutException:
+            self.click(self.locators.CREATE_CAMPAIGN_BUTTON)
