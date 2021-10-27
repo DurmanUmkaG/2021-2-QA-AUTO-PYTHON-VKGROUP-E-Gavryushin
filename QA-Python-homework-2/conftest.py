d2069f267e7b29a5a8d9aae0526dd063da71ba6e
Evgeniy Gavryushin <e.gavryushin1@gmail.com>
2021-10-27 15:42:47 +0500

    Добавил фикстуры temp_dir и logger

--- QA-Python-homework-2/conftest.py
+++ QA-Python-homework-2/conftest.py
@@ -1,4 +1,8 @@
 import os.path
+import shutil
+import sys
+
+import allure
 
 from ui.fixtures import *
 import pytest
@@ -6,12 +10,63 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption('--url', default='https://target.my.com/')
+    parser.addoption('--debug_log', action='store_true')
+
+
+def pytest_configure(config):
+    if sys.platform.startswith('win'):
+        base_dir = 'C:\\tests'
+    else:
+        base_dir = '/tmp/tests'
+
+    if not hasattr(config, 'workerinput'):
+        if os.path.exists(base_dir):
+            shutil.rmtree(base_dir)
+
+        os.makedirs(base_dir)
+
+    config.base_temp_dir = base_dir
 
 
 @pytest.fixture(scope='session')
 def config(request):
     url = request.config.getoption('--url')
-    return {'url': url}
+    debug_log = request.config.getoption('--debug_log')
+    return {'url': url, 'debug_log': debug_log}
+
+
+@pytest.fixture(scope='function')
+def temp_dir(request):
+    test_dir = os.path.join(request.config.base_temp_dir,
+                            request._pyfuncitem.nodeid.replace('/', '_').replace(':', '_'))
+
+    os.makedirs(test_dir)
+    return test_dir
+
+
+@pytest.fixture(scope='function')
+def logger(temp_dir, config):
+    log_formatter = logging.Formatter('%(asctime)s - %(filename)s - %(message)s')
+    log_file = os.path.join(temp_dir, 'test.log')
+    log_level = logging.DEBUG if config['debug_log'] else logging.INFO
+
+    file_handler = logging.FileHandler(log_file, 'w')
+    file_handler.setFormatter(log_formatter)
+    file_handler.setLevel(log_level)
+
+    log = logging.getLogger('test')
+    log.propagate = False
+    log.setLevel(log_level)
+    log.handlers.clear()
+    log.addHandler(file_handler)
+
+    yield log
+
+    for handler in log.handlers:
+        handler.close()
+
+    with open(log_file, 'r') as f:
+        allure.attach(f.read(), 'test.log', attachment_type=allure.attachment_type.TEXT)
 
 
 @pytest.fixture(scope='session')
