8548db6d65acec6a73ea3b02044c32f09446bcff
Evgeniy Gavryushin <e.gavryushin1@gmail.com>
2021-11-05 18:29:19 +0500

    Добавил тест на удаление сегмента

--- QA-Python-homework-3/tests/test_my_target_api.py
+++ QA-Python-homework-3/tests/test_my_target_api.py
@@ -8,3 +8,9 @@ class TestApi(ApiBase):
         segment_id_list = self.get_segments()
         assert segment_id in segment_id_list
         self.delete_segment(segment_id)
+
+    def test_delete_segment(self):
+        segment_id = self.create_segment()
+        self.delete_segment(segment_id)
+        segment_id_list = self.get_segments()
+        assert segment_id not in segment_id_list
